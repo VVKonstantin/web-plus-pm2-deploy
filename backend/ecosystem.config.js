37971176db6d67a05cb8c3e0b10241a6ba0d6576
Konstantin Vavilov <gravekon111@yandex.ru>
2023-08-29 11:13:42 +0500

    fix one more trivia

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -17,7 +17,7 @@ module.exports = {
       repo: DEPLOY_REPO,
       path: DEPLOY_PATH,
       'pre-deploy-local': `scp ./.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/source/backend`,
-      'post-deploy': `cd ${DEPLOY_PATH}source/backend && npm i && npm run build && && pm2 reload ecosystem.config.js && pm2 save`,
+      'post-deploy': `cd ${DEPLOY_PATH}source/backend && npm i && npm run build && pm2 start`,
     },
   },
 };
