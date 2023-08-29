3795a56f582c814bacf813d3324d00e9efd21d99
Konstantin Vavilov <gravekon111@yandex.ru>
2023-08-29 20:27:41 +0500

    fix routes in deploy

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -16,8 +16,8 @@ module.exports = {
       ref: DEPLOY_REF,
       repo: DEPLOY_REPO,
       path: DEPLOY_PATH,
-      'pre-deploy-local': `scp ./.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/source/backend`,
-      'post-deploy': `cd ${DEPLOY_PATH}source/backend && npm i && npm run build && pm2 start`,
+      'pre-deploy-local': `scp -Cr .env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}backend`,
+      'post-deploy': `cd ${DEPLOY_PATH}backend && npm i && npm run build && pm2 start`,
     },
   },
 };
