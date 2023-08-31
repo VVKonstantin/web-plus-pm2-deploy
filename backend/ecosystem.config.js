b9a3f6e13c656fb2ad08ee8b51be99449c8d772b
Konstantin Vavilov <gravekon111@yandex.ru>
2023-08-31 11:15:49 +0500

    fix paths

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -16,8 +16,8 @@ module.exports = {
       ref: DEPLOY_REF,
       repo: DEPLOY_REPO,
       path: DEPLOY_PATH,
-      'pre-deploy-local': `scp -Cr .env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
-      'post-deploy': `cd ${DEPLOY_PATH} && npm i && npm run build && pm2 startOrRestart ecosystem.config.js --env production`,
+      'pre-deploy-local': `scp -Cr .env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/source/backend`,
+      'post-deploy': `cd ${DEPLOY_PATH}/source/backend && npm i && npm run build && pm2 startOrRestart ecosystem.config.js --env production`,
     },
   },
 };
